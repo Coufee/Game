6a88b6480bc1ebbd30b561f0c3f938f44d1e072e
coufee <631387795@qq.com>
2018-11-01 15:55:25 +0800

    1.

--- tool/common/common.go
+++
@@ -1,7 +0,0 @@
-package common
-
-const (
-	HEART_BEAT			= 10
-	TIMER_QUENE_SIZE	= 1024
-	TIME_OUT_FACTER		= 3
-)
--- tool/zebra/tcp_client.go
+++
@@ -1,8 +0,0 @@
-package zebra
-
-import "time"
-
-//启动 tcp client
-func TCPClientServer(se Sessioner, conf *Config, timeout time.Duration) bool {
-	return newBroker(se, conf).Connect(timeout)
-}
